30f634ea375d974aa696d542d7f224652c722fd8
Fanicot <facokpo@gmail.com>
2024-10-24 03:43:05 -0300

    Movimiento Enemigo
    Patrulla y deteccion

---
+++ Proyecto RPG/Assets/scripts/EnemigoMov.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoMov : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] PuntosPatrulla;
+    [SerializeField]
+    private int PuntoActual = 0;
+    private NavMeshAgent enemigo;
+    [SerializeField]
+    private Transform jugador;
+    [SerializeField]
+    private bool persiguiendo = false;
+    [SerializeField]
+    private bool jugadorEnRango = false;
+
+
+    void Start()
+    {
+        enemigo = GetComponent<NavMeshAgent>();
+        enemigo.autoBraking = false;
+
+        IrAlSiguientePunto();
+    }
+
+    void Update()
+    {
+        if (jugadorEnRango)
+            PerseguirJugador();
+
+        if (!enemigo.pathPending && enemigo.remainingDistance < 0.5f)
+        {
+            IrAlSiguientePunto();
+        }
+    }
+
+    public void IrAlSiguientePunto()
+    {
+        if (PuntosPatrulla.Length == 0) return;
+
+        enemigo.destination = PuntosPatrulla[PuntoActual].position;
+
+        PuntoActual = (PuntoActual + 1) % PuntosPatrulla.Length;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorEnRango = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            jugadorEnRango = false;
+            persiguiendo = false;
+            IrAlSiguientePunto();
+        }
+    }
+
+    public void PerseguirJugador()
+    {
+        persiguiendo = true;
+        enemigo.destination = jugador.position;
+    }
+}
